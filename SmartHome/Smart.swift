15711b93ae5fea0075cb54adac4fd79d0ae953b9
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2025-01-11 13:59:43 +0530

    add image and create file

---
+++ SmartHome/Smart.swift
@@ -0,0 +1,18 @@
+//
+//  Smart.swift
+//  SmartHome
+//
+//  Created by Bhumika Patel on 11/01/25.
+//
+
+import SwiftUI
+
+struct Smart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Smart()
+}
---
+++ SmartHome/SmartHome.swift
@@ -0,0 +1,18 @@
+//
+//  SmartHome.swift
+//  SmartHome
+//
+//  Created by Bhumika Patel on 11/01/25.
+//
+
+import SwiftUI
+
+struct SmartHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SmartHome()
+}
